c5f7e6008e66fc25a7dce1d130121cdf7d455e9c
Dinesh-1206 <kandpaldiinesh668@gmail.com>
2026-02-11 20:39:16 +0530

    push

--- 03basics/01_funcions.js
+++ 03basics/01_funcions.js
@@ -15,6 +15,8 @@ function addTwoNumbers(num1,num2){
 
 addTwoNumbers(3,4);
 
+/************************************************** */
+
 function loginUserMessage(username="sam"){
     if(!username){
         return "Please enter username"
@@ -23,3 +25,30 @@ function loginUserMessage(username="sam"){
     return`${username} just logged in`
 }
 console.log(loginUserMessage());
+
+/*************************************************/
+
+function calculateCartPrice(...prices){ //rest operator
+    return prices;
+}
+console.log(calculateCartPrice(20,30,40,50));
+
+//*************************************************/
+
+const user={
+    Item:"Shirt",
+    price:999
+}
+
+function handleObject(anyObject){
+    console.log(`Item is ${anyObject.Item} and price is ${anyObject.price}`);
+}
+handleObject(user);
+
+//**************************************
+
+const myNewArray=[1,2,3,4,5];
+function returnSecondValue(array){
+    return array[0];
+}
+console.log(returnSecondValue(myNewArray));
